d21b63a8d15fe2d9bb86ca82736dcd63bae3aeda
pjwilliams2 <williams.patrick.j@gmail.com>
2020-04-12 18:40:09 -0400

    Traditional colors

--- src/components/GamePieces.js
+++ src/components/GamePieces.js
@@ -1,21 +1,21 @@
 const pieceTemplates = {
     square: [
-        { x: -1, y: -1, color: 'blue'},
-        { x: 0, y: -1, color: 'blue'},
-        { x: -1, y: 0, color: 'blue'},
-        { x: 0, y: 0, color: 'blue'},
+        { x: -1, y: -1, color: 'yellow'},
+        { x: 0, y: -1, color: 'yellow'},
+        { x: -1, y: 0, color: 'yellow'},
+        { x: 0, y: 0, color: 'yellow'},
     ],
     line: [
-        { x: 0, y: 0, color: 'green'},
-        { x: 0, y: -1, color: 'green'},
-        { x: 0, y: -2, color: 'green'},
-        { x: 0, y: -3, color: 'green'},
+        { x: 0, y: 0, color: 'lightblue'},
+        { x: 0, y: -1, color: 'lightblue'},
+        { x: 0, y: -2, color: 'lightblue'},
+        { x: 0, y: -3, color: 'lightblue'},
     ],
     elShapeLeft: [
-        { x: 0, y: -2, color: 'orange'},
-        { x: 0, y: -1, color: 'orange'},
-        { x: 0, y: 0, color: 'orange'},
-        { x: -1, y: 0, color: 'orange'},
+        { x: 0, y: -2, color: 'darkblue'},
+        { x: 0, y: -1, color: 'darkblue'},
+        { x: 0, y: 0, color: 'darkblue'},
+        { x: -1, y: 0, color: 'darkblue'},
     ],
     elShapeRight: [
         { x: 0, y: -2, color: 'orange'},
@@ -24,22 +24,22 @@ const pieceTemplates = {
         { x: 1, y: 0, color: 'orange'},
     ],
     teeShape: [
-        { x: 0, y: 0, color: 'red' },
-        { x: -1, y: 1, color: 'red' },
-        { x: 0, y: 1, color: 'red' },
-        { x: 1, y: 1, color: 'red' }
+        { x: 0, y: 0, color: 'purple' },
+        { x: -1, y: 1, color: 'purple' },
+        { x: 0, y: 1, color: 'purple' },
+        { x: 1, y: 1, color: 'purple' }
     ],
     zigZagLeft: [
-        { x: -1, y: -1, color: 'violet'},
-        { x: 0, y: -1, color: 'violet'},
-        { x: 0, y: 0, color: 'violet'},
-        { x: 1, y: 0, color: 'violet'},
+        { x: -1, y: -1, color: 'red'},
+        { x: 0, y: -1, color: 'red'},
+        { x: 0, y: 0, color: 'red'},
+        { x: 1, y: 0, color: 'red'},
     ],
     zigZagRight: [
-        { x: 1, y: -1, color: 'violet'},
-        { x: 0, y: -1, color: 'violet'},
-        { x: 0, y: 0, color: 'violet'},
-        { x: -1, y: 0, color: 'violet'},
+        { x: 1, y: -1, color: 'green'},
+        { x: 0, y: -1, color: 'green'},
+        { x: 0, y: 0, color: 'green'},
+        { x: -1, y: 0, color: 'green'},
     ]
 };
 
